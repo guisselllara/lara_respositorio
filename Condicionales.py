4c3d6d8bf338fa7a2ab4a7ccb185e9a0541acb7b
guisellara <guisselllara14@gmail.com>
2025-02-18 14:12:44 -0600

    confirmacion

---
+++ Condicionales.py
@@ -0,0 +1,16 @@
+edad=18
+#Condicionales
+if edad>=18:
+    print("puedes entrar a la fiesta")
+
+#Realizar un programa que valida si una persona es menos de edad
+#debe mostrar un mensaje que indique que es un menos de edad
+
+#pedir la edad de la persona
+edad = int(input("Por favor, ingresa tu edad: "))
+
+# Comprobar si la persona es menor de edad
+if edad < 18:
+    print("Eres menor de edad.")
+else:
+    print("Eres mayor de edad.")
