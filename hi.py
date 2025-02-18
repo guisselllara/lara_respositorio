c144478fc4ca811b671d302d81ce5d73a2ab8eef
guisellara <guisselllara14@gmail.com>
2025-02-18 17:43:51 -0600

    listo

---
+++ hi.py
@@ -0,0 +1,5 @@
+#se necesita un programa que se muestre un mensaje, segun
+#elbrango de calificaciones, 0-59, aplazado,
+#60-69 regular, 70-79 bueno, 80-89 muy bueno,
+#90-100 excelente
+
---
+++ if.py
@@ -0,0 +1,21 @@
+#Condicional
+
+#Se necesita un programa que valida si
+#UNa persona es mayor de edad po es menor de edad
+#si es mayor, mostrar un mensaje que diga "es mayor de edad"
+#si es menor de edad, mostrar un mensaje que diga " es menor de edad"
+
+edad = 18
+
+if edad>= 18:
+    print("es mayor de edad")
+
+elif edad >=14:
+    print("Es adolecente")
+    
+elif edad >=10:
+    print("Es infante")
+    
+else:
+    print("No llega ni a 10, es mejor nque no te acerques")
+       
