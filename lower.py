f00f0cbe0b3f2909efcd0aca8cff671007df6e22
guisellara <guisselllara14@gmail.com>
2025-02-20 16:00:34 -0600

    todo listo

---
+++ lower.py
@@ -0,0 +1,5 @@
+nombre_1 = "heymi lara"
+nombre_2 = "guissel chavarria"
+
+print(nombre_1.upper())
+print(nombre_2.lower())
---
+++ upper.py
@@ -0,0 +1,3 @@
+nombre = "lara"
+print(f"lara sin aplicar el metodo upper: {nombre}")
+print(f"lara aplicando el metodo upper: {nombre.upper()}")
